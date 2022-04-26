a5be4fd3d6f9aa0f43608c7b6cc2cd2c616156e2
rthompson17 <ryann.thompson@gmail.com>
2022-04-26 17:11:41 -0400

    up to displaying a feeding or eveng but no menu item to attach a Thing to Events in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
+from .models import Thing, Events
 
 # Register your models here.
-from django.contrib import admin
-from .models import Thing
 admin.site.register(Thing)
-
+admin.site.register(Events)
--- main_app/models.py
+++ main_app/models.py
@@ -14,3 +14,25 @@ class Thing(models.Model):
       # Add this method
     def get_absolute_url(self):
         return reverse('detail', kwargs={'thing_id': self.id})
+
+TYPES = (
+    ('N', 'Nature'),
+    ('V', 'Visit'),
+    ('T', 'Tour')
+)
+
+class Events(models.Model):
+  date = models.DateField()
+  type = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=TYPES,
+    # set the default value for meal to be 'B'
+    default=TYPES[0][0]
+  )
+
+# Create a thing ForeignKey (in psql)
+thing = models.ForeignKey(Thing, on_delete=models.CASCADE)
+
+def __str__(self):
+      return f"{self.get_type_display()} on {self.date}"
